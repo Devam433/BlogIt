c5af7d861f2f517906e1ebde47d736b77ac210e7
Devam <devam433@gmail.com>
2024-04-13 20:14:25 +0530

    minor changes

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import { Link, NavLink, Outlet } from 'react-router-dom'
+import { BlogCard } from '../Components/BlogCard'
+import { SearchBlog } from '../Components/SearchBlog'
 
 export const Blogs = () => {
-  const blogs =[1,2,3,4,5,6];
   return (
-    <div className=''>
-    <div className='flex flex-col gap-2'>
-      {blogs.map(blog=><NavLink key={crypto.randomUUID()} to={`/blogs/${blog}`} className={({isActive})=>
-        isActive ? `text-red-500` : ''
-      }>Blog {blog}</NavLink>)}
-    </div>      
-    <Outlet />
-    </div>
+    // <div className=''>
+    // <div className='flex flex-col gap-2'>
+    //   {blogs.map(blog=><NavLink key={crypto.randomUUID()} to={`/blogs/${blog}`} className={({isActive})=>
+    //     isActive ? `text-red-500` : ''
+    //   }>Blog {blog}</NavLink>)}
+    // </div>      
+    // <Outlet />
+    // </div>
 
-    
+    <div className='w-[full] flex justify-center'>
+      <div className='w-[1210px]'>
+        <main className=' py-3'>
+          <SearchBlog />
+          <section className='py-8'>
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+          </section>
+        </main>
+        
+      </div>
+    </div>
   )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Hero } from '../Components/Hero'
+import { CategoryCard } from '../Components/CategoryCard'
 
 export const Home = () => {
   return (
-    <div> I am Home</div>
+    <div className='w-[full] flex justify-center'>
+      <div className='w-[1210px]'>
+        <Hero />
+        <CategoryCard />
+      </div>
+    </div>
   )
 }
